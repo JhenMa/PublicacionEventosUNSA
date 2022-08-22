d8817dfcac56ce5d92266d04c0113ce7646f624f
JhenMa <71655718+JhenMa@users.noreply.github.com>
2022-08-22 17:03:06 -0500

    Add files via upload

---
+++ blogex/models/agente/Administrador.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Administrador:
+    def __init__(self):
+        self.id_admin = None
+        self.Nombre = None
+
+    def ValidarCuenta(self, ):
+        pass
+
+    def AprobarEvento(self, ):
+        pass
+
+    def EntrevistarUsuario(self, ):
+        pass
+
+    def RegistrarEvento(self, ):
+        pass
+
+    def RegistrarUsuario(self, ):
+        pass
+
